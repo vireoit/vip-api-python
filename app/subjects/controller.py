3abaa845790c6b4b9be9b09400e91b6bfd00f29c
CYCLOIDES\CY0166 <arunya.j@cycloides.co.in>
2021-10-08 13:20:53 +0530

    controllers, delegates, schemas and services included
    Signed-off-by: CYCLOIDES\CY0166 <arunya.j@cycloides.co.in>

---
+++ app/subjects/controller.py
@@ -0,0 +1,25 @@
+import requests
+from flask import request
+from flask_jwt_extended import get_jwt, jwt_required
+from flask_restx import Namespace, Resource
+from marshmallow import ValidationError
+from flask import Response as flask_response
+from app.response import Response
+from app.status_constants import HttpStatusCode
+
+import json
+
+api = Namespace("VIP", description="Namespace for VIP API Services")
+
+
+@api.route("/metadata")
+class MetaData(Resource):
+    """
+    Class for handle CoOpJobs end points
+    """
+    @permission_required("GET_CONFIG")
+    def get(self):
+        """
+        Return all jobs
+        """
+        return Response.error(err.messages, HttpStatusCode.BAD_REQUEST)
