87e4fc8ab38ab2373628da1117db22b95726412b
in-vishnu-prasad <vishnu.p@inscholaris.com>
2021-10-14 15:54:54 +0530

    Added API for Add, Update and Delete of events

--- app/masters/controller.py
+++ app/masters/controller.py
@@ -1,3 +1,4 @@
+from pymongo.message import delete
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_restx import Namespace, Resource
 from marshmallow import ValidationError
@@ -10,7 +11,8 @@ from app.status_constants import HttpStatusCode
 from app.exceptions import FileNotSelected, FileUploadException, FileFormatException
 from app.utils import file_service_util
 # from app import constants
-from app.masters.delegates import AdminListDelegate
+from app.masters.delegates import AdminListDelegate, MasterEventDelegate
+from app.masters.schemas import MasterEventSchema
 from flask_restx import Api, Resource, fields
 
 
@@ -42,3 +44,74 @@ class MasterAdmin(Resource):
                                 status_code=HttpStatusCode.OK,
                                 message="Admin list fetched succesfully")
 
+
+@api.route("/master/event")
+class MasterEvent(Resource):
+    # @jwt_required
+    # def get(self):
+    #     """
+    #         API for list all event for master
+    #     """
+    #     parameters = {
+    #         'limit': 10,
+    #         'order': 'desc'
+    #     }
+    #     if 'limit' in request.args and request.args.get('limit'):
+    #         parameters['limit'] = int(request.args.get('limit'))
+    #     if 'page_size' in request.args and request.args.get('page_size'):
+    #         parameters['page_size'] = int(request.args.get('page_size'))
+    #     if 'order' in request.args and request.args.get('order'):
+    #         parameters['order'] = str(request.args.get('order'))
+    #     if 'search' in request.args and request.args.get('search'):
+    #         parameters['search'] = str(request.args.get('search'))
+        
+    #     admin_data = MasterEventListAddDelegate.get_event_list(parameters)
+    #     return Response.success(response_data=admin_data,
+    #                             status_code=HttpStatusCode.OK,
+    #                             message="Event list fetched succesfully")
+
+    # @jwt_required
+    def post(self):
+        """
+        API to add events on master
+        """
+        try:
+            payload = request.json
+            MasterEventSchema().load(payload)
+            data = MasterEventDelegate.add_master_event(payload)
+            return Response.success(response_data={},
+                        status_code=HttpStatusCode.OK,
+                        message="Event successfully saved")
+        except ValidationError as err:
+            return Response.error(next(iter(err.messages.values())), HttpStatusCode.BAD_REQUEST, message='Event saving failed')
+
+#     @jwt_required
+    def put(self):
+        """
+        API to update events on master
+        """
+        try:
+            payload = request.json
+            event_id = payload.get('id')
+            payload.pop('id')
+            MasterEventSchema().load(payload)
+            data = MasterEventDelegate.update_master_event(payload, event_id)
+            return Response.success(response_data={},
+                        status_code=HttpStatusCode.OK,
+                        message="Event successfully updated")
+        except ValidationError as err:
+            return Response.error(next(iter(err.messages.values())), HttpStatusCode.BAD_REQUEST, message='Event updating failed')
+    
+    # @jwt_required
+    def delete(self):
+        """
+        API to delete events on master
+        """
+        payload = {
+            'id': request.args.get('id')
+        }
+        data = MasterEventDelegate.delete_master_event(payload)
+        return Response.success(response_data={},
+                    status_code=HttpStatusCode.OK,
+                    message="Event successfully deleted")
+        
--- app/masters/delegates.py
+++ app/masters/delegates.py
@@ -1,4 +1,5 @@
-from app.masters.services import AdminListService
+from app.masters.services import AdminListService, MasterEventService
+from app import response
 
 
 class AdminListDelegate:
@@ -7,3 +8,23 @@ class AdminListDelegate:
         data = AdminListService.get_admin_list(parameters)
         return data
         
+class MasterEventDelegate:
+    # @staticmethod
+    # def get_event_list(payload):
+    #     response = MasterEventService.get_event_list(payload)
+    #     return response
+
+    @staticmethod
+    def add_master_event(payload):
+        response = MasterEventService.add_master_event(payload)
+        return response
+
+    @staticmethod
+    def update_master_event(payload, event_id):
+        response = MasterEventService.update_master_event(payload, event_id)
+        return response
+
+    @staticmethod
+    def delete_master_event(payload):
+        response = MasterEventService.delete_master_event(payload)
+        return response
