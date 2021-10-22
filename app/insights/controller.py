74e2d334740a2f2a7271e490af2bc36a12e2f86e
CYCLOIDES\CY0166 <arunya.j@cycloides.co.in>
2021-10-22 15:25:48 +0530

    API for peg score graph created
    Signed-off-by: CYCLOIDES\CY0166 <arunya.j@cycloides.co.in>

--- app/insights/controller.py
+++ app/insights/controller.py
@@ -12,10 +12,37 @@ from app.exceptions import FileNotSelected, FileUploadException, FileFormatExcep
 from app.utils import file_service_util
 from app.home.schemas import PegScore, FeedBack
 # from app import constants
-from app.home.delegates import PegScoreDelegate, OnGoingFeedBack
+from app.insights.delegates import PainDetailGraphDelegate
 from flask_restx import Api, Resource, fields
 
-api = Namespace("Home", description="Namespace for Home")
+api = Namespace("Insights", description="Namespace for Insights")
+
+@api.doc(params={'subject': 'ID of the Subject - 60bb10c89cf5432080d40346 ', "param": "today" "for today filter",
+                 "param":"week" "for 7 days filter", "param":"month" "for 30 days filter"})
+@api.route("/insights/personal/pain")
+class PainDetailGraph(Resource):
+    """
+    Class for populate pain score based graph data
+    """
+    @jwt_required()
+    def get(self):
+        """
+        Return all pain score graph values
+        """
+        claims = ""
+        parameters = {
+            'subject': "",
+            'date': "",
+        }
+
+        if 'subject' in request.args and request.args.get('subject'):
+            parameters['subject'] = request.args.get('subject')
+        if 'param' in request.args and request.args.get('param'):
+            parameters['param'] = request.args.get('param')
+
+        data = PainDetailGraphDelegate.pain_details_graph(filters=parameters, user_identity=claims)
+        return Response.success(response_data=data,
+                                status_code=HttpStatusCode.OK, message="Peg score related data")
 
 
 
---
+++ app/insights/services.py
@@ -0,0 +1,72 @@
+from app import ma, response, mongo_db
+from app.utils.mongo_encoder import format_cursor_obj
+
+import json
+
+from datetime import datetime, timedelta, date
+
+from bson.objectid import ObjectId
+from bson.json_util import dumps, RELAXED_JSON_OPTIONS
+
+
+class PainDetailGraphService:
+    @staticmethod
+    def pain_details_graph(filters, user_identity):
+        subject = filters['subject'] if 'subject' in filters else ""
+        param = filters['param'] if 'param' in filters else ""
+        all_data = []
+        if param == "today":
+            date_today = date.today()
+            start_date = datetime.strptime(str(date_today) + " 00", "%Y-%m-%d %H")
+            end_date = datetime.strptime(str(date_today) + " 23", "%Y-%m-%d %H")
+            query_data = list(mongo_db.db.Pegs.find({"AddedOn": {"$lte": end_date, '$gte': start_date},
+                                                     "SubjectId": ObjectId(subject), "IsActive": True}). \
+                              sort("AddedOn", -1))
+            data = query_data[0]
+            dict={}
+            dict['score'] = data['Percentage']
+            dict['date'] = data['AddedOn'].strftime('%m-%d-%Y')
+            all_data.append(dict)
+        elif param == "week":
+            date_today = date.today()
+            week_ago = date_today - timedelta(days=7)
+            start_date = datetime.strptime(str(week_ago) + " 00", "%Y-%m-%d %H")
+            end_date = datetime.strptime(str(date_today) + " 23", "%Y-%m-%d %H")
+            query_data = list(mongo_db.db.Pegs.find({"AddedOn": {"$lte": end_date, '$gte': start_date},
+                                                     "SubjectId": ObjectId(subject), "IsActive": True}). \
+                              sort("AddedOn", -1))
+            for data in query_data:
+                dict = {}
+                dict['score'] = data['Percentage']
+                dict['date'] = data['AddedOn'].strftime('%m-%d-%Y')
+                all_data.append(dict)
+                subject = data['SubjectId']
+                in_date = data['AddedOn'].strftime('%Y-%m-%d')
+                for record in query_data:
+                    if record['SubjectId'] == subject and record['AddedOn'].strftime('%Y-%m-%d') == in_date:
+                        query_data.remove(record)
+
+        elif param == "month":
+            date_today = date.today()
+            month_ago = date_today - timedelta(days=30)
+            start_date = datetime.strptime(str(month_ago) + " 00", "%Y-%m-%d %H")
+            end_date = datetime.strptime(str(date_today) + " 23", "%Y-%m-%d %H")
+            query_data = list(mongo_db.db.Pegs.find({"AddedOn": {"$lte": end_date, '$gte': start_date},
+                                                     "SubjectId": ObjectId(subject), "IsActive": True}). \
+                              sort("AddedOn", -1))
+            all_data = []
+            for data in query_data:
+                dict = {}
+                dict['score'] = data['Percentage']
+                dict['date'] = data['AddedOn'].strftime('%m-%d-%Y')
+                all_data.append(dict)
+                subject = data['SubjectId']
+                in_date = data['AddedOn'].strftime('%Y-%m-%d')
+                for record in query_data:
+                    if record['SubjectId'] == subject and record['AddedOn'].strftime('%Y-%m-%d') == in_date:
+                        query_data.remove(record)
+
+        return all_data
+
+
+
