2751ee90e55de09503a31056f8fda4e6ba301464
CYCLOIDES\CY0166 <arunya.j@cycloides.co.in>
2021-11-02 18:22:18 +0530

    Environment created for QA, dockerfile created
    Signed-off-by: CYCLOIDES\CY0166 <arunya.j@cycloides.co.in>

--- app/base_urls.py
+++ app/base_urls.py
@@ -1,3 +1,13 @@
-#Admin dev url
+import os
+
+env = os.getenv("FLASK_ENV") or "dev"
 VIP_ADMIN_URL = "https://103.79.223.59:9094"
 VIP_BACKEND_URL = "https://103.79.223.59:1013"
+
+
+if env == "test":
+    VIP_ADMIN_URL = "https://103.79.223.59:9094"
+    VIP_BACKEND_URL = "https://103.79.223.59:9094"
+
+
+
