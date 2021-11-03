88d5db86764f6c46735f010b1deef255749033f2
in-vishnu-prasad <vishnu.p@inscholaris.com>
2021-11-03 15:30:54 +0530

    Survey report updated

--- app/surveys/services.py
+++ app/surveys/services.py
@@ -20,19 +20,19 @@ class SurveyService:
             response_data = perform_http_request(f'{VIP_BACKEND_URL}/api/Surveys/Reports', parameters['authorization'], 
                 body=data, request_method="POST")
             is_valid = response_data.get('responseCode') == 200 and response_data.get('data')
+            response_list = []
+            all_data_list = []
             if is_valid:
                 all_data = response_data.get('data')
+                dict={}
                 for data in all_data:
-                    data['Question'] = data.pop('question')
-                    data['Subject Name'] = data.pop('subjectName')
-                    data['Submitted Date'] = data.pop('submittedDate')
-                    data['Answer'] = data.pop('answer')
-                    del data['title']
-                    del data['imagePath']
-                    del data['description']
-            else:
-                all_data = []
-            data_file = export_table_data(all_data)
+                    dict['Subject Name'] = data['subjectName']
+                    dict['Submitted Date'] = data['submittedDate']
+                    dict[data['question']] = data['answer']
+                    response_list.append(dict)
+                response_dict = {k:v for x in response_list for k,v in x.items()}
+                all_data_list.append(response_dict)
+            data_file = export_table_data(all_data_list)
             return data_file
         except:
             return None
