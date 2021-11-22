d370bde21aadf07366a633585d0869f239081967
in-rajesh-bk <rajesh.b@inscholaris.com>
2021-11-22 16:52:54 +0530

    Scheduled jobs added

--- app/base_urls.py
+++ app/base_urls.py
@@ -4,9 +4,11 @@ env = os.getenv("FLASK_ENV") or "dev"
 VIP_ADMIN_URL = "https://103.79.223.59:9094"
 VIP_BACKEND_URL = "https://103.79.223.59:1013"
 VIP_EMAIL_LINK = "103.79.223.61:2561"
+PASSWORD_RESET_LINK = "/api/Subject/ResetPassword"
 
 if env == "test":
     VIP_ADMIN_URL = "https://103.79.223.59:9094"
     VIP_BACKEND_URL = "https://103.79.223.59:9094"
     VIP_EMAIL_LINK = "103.79.223.61:2561"
+
 
